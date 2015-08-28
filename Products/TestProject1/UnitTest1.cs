ae3c54bb98849ba672a61c4f730236b87cb8e5f9
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-08-28 10:57:50 +0200

    BoxSizePassTestFinished

---
+++ Products/TestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Products;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestOfBoxSizePrintPass()
+        {
+            Size s = new BoxSize(1,1,1);
+            string text = s.GetAsText();
+            Assert.AreEqual("1x1x1 mm", text);
+        }
+    }
+}
